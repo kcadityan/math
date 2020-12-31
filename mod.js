588d50dfe9a2748b606b941779cbd70ba49d8d44
kcadityan <kcadityan@gmail.com>
2020-12-31 16:54:54 +0530

    added 2 fun

---
+++ mod.js
@@ -0,0 +1,4 @@
+function mod (a,b){
+    return (a % b)
+}
+console.log (mod(5,4), mod(5,3), mod(20,10))
---
+++ square.js
@@ -0,0 +1,7 @@
+function square (a){
+    return a*a
+}
+
+const s =square(square(5))
+console.log (square(5),s)
+
